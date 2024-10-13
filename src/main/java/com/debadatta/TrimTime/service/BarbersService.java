56139234eab30b6e65dc965147895f6c021ac929
Deba43 <debadatta016@gmail.com>
2024-10-14 00:04:33 +0530

    Create Barbers Profile

--- src/main/java/com/debadatta/TrimTime/service/BarbersService.java
+++ src/main/java/com/debadatta/TrimTime/service/BarbersService.java
@@ -3,6 +3,7 @@ package com.debadatta.TrimTime.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.debadatta.TrimTime.model.Barbers;
 import com.debadatta.TrimTime.repo.BarbersRepo;
 
 import lombok.AllArgsConstructor;
@@ -13,5 +14,9 @@ public class BarbersService {
     
     @Autowired
     private BarbersRepo barbersRepo;
+
+    public String createProfile(Barbers barbers) {
+      return barbersRepo.createProfile(barbers);
+    }
     
 }
