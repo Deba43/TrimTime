2c591b80db1fb6c1c4a2128697e6a549894cf90c
Deba43 <debadatta016@gmail.com>
2024-10-14 00:03:16 +0530

    Create Barbers Profile

--- src/main/java/com/debadatta/TrimTime/controller/BarbersController.java
+++ src/main/java/com/debadatta/TrimTime/controller/BarbersController.java
@@ -1,11 +1,16 @@
 package com.debadatta.TrimTime.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.debadatta.TrimTime.model.Barbers;
 import com.debadatta.TrimTime.service.BarbersService;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -17,6 +22,12 @@ public class BarbersController {
     private BarbersService barberService;
 
     //createProfile
+    @PostMapping("/createProfile")
+    public ResponseEntity<String> createProfile(@Valid @RequestBody Barbers barbers){
+        String barberId =  barberService.createProfile(barbers);
+        return ResponseEntity.ok(barberId);
+    }
+
     //updateProfile
     //deleteProfile
     //viewAllAppointments
