ccec4e59c6f7118c5e0553722a4994a418b62406
Deba43 <debadatta016@gmail.com>
2024-10-14 23:10:00 +0530

    findByBarberId

--- src/main/java/com/debadatta/TrimTime/repo/ReviewsRepo.java
+++ src/main/java/com/debadatta/TrimTime/repo/ReviewsRepo.java
@@ -1,9 +1,16 @@
 package com.debadatta.TrimTime.repo;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.debadatta.TrimTime.model.Reviews;
 
 import lombok.AllArgsConstructor;
 
@@ -13,5 +20,16 @@ public class ReviewsRepo {
 
     @Autowired
     final private DynamoDBMapper dynamoDBMapper;
+
+    public List<Reviews> findByBarberId(String barber_id) {
+        Map<String, AttributeValue> eav = new HashMap<>();
+        eav.put(":barber_id", new AttributeValue().withS(barber_id));
+
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withFilterExpression("barber_id = :barber_id")
+                .withExpressionAttributeValues(eav);
+
+        return dynamoDBMapper.scan(Reviews.class, scanExpression);
+    }
 
 }
