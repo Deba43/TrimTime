5c1f4d8ec555303e2ce69467db2887d579c7b4c6
Deba43 <debadatta016@gmail.com>
2024-10-14 23:01:22 +0530

    getAllAppointments, getAppointmentsForBarber

--- src/main/java/com/debadatta/TrimTime/service/AppointmentsService.java
+++ src/main/java/com/debadatta/TrimTime/service/AppointmentsService.java
@@ -1,8 +1,11 @@
 package com.debadatta.TrimTime.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.debadatta.TrimTime.model.Appointments;
 import com.debadatta.TrimTime.repo.AppointmentsRepo;
 
 import lombok.AllArgsConstructor;
@@ -13,5 +16,13 @@ public class AppointmentsService {
 
     @Autowired
     private AppointmentsRepo appointmentsRepo;
+
+     public List<Appointments> getAllAppointments() {
+        return appointmentsRepo.fetchAllAppointments();
+    }
+
+    public List<Appointments> getAppointmentsForBarber(String barber_id) {
+        return appointmentsRepo.fetchAppointmentsForBarber(barber_id);
+    }
     
 }
