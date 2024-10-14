63cc26f2c55b824469ee8277202552cf939c6ef9
Deba43 <debadatta016@gmail.com>
2024-10-14 23:02:29 +0530

    create/update/delete Profile

--- src/main/java/com/debadatta/TrimTime/service/BarbersService.java
+++ src/main/java/com/debadatta/TrimTime/service/BarbersService.java
@@ -11,12 +11,22 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class BarbersService {
-    
-    @Autowired
-    private BarbersRepo barbersRepo;
 
-    public String createProfile(Barbers barbers) {
-      return barbersRepo.createProfile(barbers);
-    }
-    
+  @Autowired
+  private BarbersRepo barbersRepo;
+
+  public String createProfile(Barbers barbers) {
+    return barbersRepo.createProfile(barbers);
+  }
+
+  public Barbers updateProfile(String barber_id, Barbers barbers) {
+    return barbersRepo.updateProfile(barber_id, barbers);
+
+  }
+
+  public String deleteProfile(String barber_id) {
+    return barbersRepo.deleteProfile(barber_id);
+
+  }
+
 }
