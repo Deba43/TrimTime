1bd5b99d1d468177c47aa29315f0afd25f634980
Deba43 <debadatta016@gmail.com>
2024-10-14 23:03:20 +0530

    create/update/delete Profile

--- src/main/java/com/debadatta/TrimTime/service/CustomersService.java
+++ src/main/java/com/debadatta/TrimTime/service/CustomersService.java
@@ -3,6 +3,7 @@ package com.debadatta.TrimTime.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.debadatta.TrimTime.model.Customers;
 import com.debadatta.TrimTime.repo.CustomersRepo;
 
 import lombok.AllArgsConstructor;
@@ -11,7 +12,21 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class CustomersService {
 
-    @Autowired
-    private CustomersRepo customersRepo;
-    
+  @Autowired
+  private CustomersRepo customersRepo;
+
+  public String createProfile(Customers customers) {
+    return customersRepo.createProfile(customers);
+  }
+
+  public Customers updateProfile(String customer_id, Customers customers) {
+    return customersRepo.updateProfile(customer_id, customers);
+
+  }
+
+  public String deleteProfile(String customer_id) {
+    return customersRepo.deleteProfile(customer_id);
+
+  }
+
 }
