c609fbbe2f6a08f6b0dc1d319aebff80dd957c6e
Deba43 <debadatta016@gmail.com>
2024-10-14 23:08:13 +0530

    create/update/delete Profile

--- src/main/java/com/debadatta/TrimTime/repo/BarbersRepo.java
+++ src/main/java/com/debadatta/TrimTime/repo/BarbersRepo.java
@@ -11,13 +11,55 @@ import lombok.AllArgsConstructor;
 @Repository
 @AllArgsConstructor
 public class BarbersRepo {
-    
+
     @Autowired
     final private DynamoDBMapper dynamoDBMapper;
 
     public String createProfile(Barbers barbers) {
-         dynamoDBMapper.save(barbers);
-         return barbers.getBarber_id();
+        dynamoDBMapper.save(barbers);
+        return barbers.getBarber_id();
     }
-    
+
+    public Barbers updateProfile(String barber_id, Barbers barbers) {
+        Barbers existingBarbers = dynamoDBMapper.load(Barbers.class, barber_id);
+
+        if (existingBarbers == null) {
+            throw new RuntimeException("Your ID " + barber_id + " not found");
+        }
+        if (barbers.getName() != null)
+            existingBarbers.setName(barbers.getName());
+        if (barbers.getMobileNumber() != null)
+            existingBarbers.setMobileNumber(barbers.getMobileNumber());
+        if (barbers.getPassword() != null)
+            existingBarbers.setPassword(barbers.getPassword());
+        if (barbers.getBarberShopName() != null)
+            existingBarbers.setBarberShopName(barbers.getBarberShopName());
+        if (barbers.getLocation() != null)
+            existingBarbers.setLocation(barbers.getLocation());
+        if (barbers.getIsAvailable() != null)
+            existingBarbers.setIsAvailable(barbers.getIsAvailable());
+        if (barbers.getExperience() != null)
+            existingBarbers.setExperience(barbers.getExperience());
+        if (barbers.getRole() != null)
+            existingBarbers.setRole(barbers.getRole());
+        if (barbers.getBio() != null)
+            existingBarbers.setBio(barbers.getBio());
+        if (barbers.getProfilePictureUrl() != null)
+            existingBarbers.setProfilePictureUrl(barbers.getProfilePictureUrl());
+
+        dynamoDBMapper.save(existingBarbers);
+
+        return dynamoDBMapper.load(Barbers.class, barber_id);
+
+    }
+
+    public String deleteProfile(String barber_id) {
+        Barbers exitingBarbers = dynamoDBMapper.load(Barbers.class, barber_id);
+        if (exitingBarbers == null) {
+            throw new RuntimeException("Your ID " + barber_id + " not found");
+        }
+        dynamoDBMapper.delete(exitingBarbers);
+        return "Barber profile for " + exitingBarbers.getName() + " has been deleted successfully.";
+    }
+
 }
