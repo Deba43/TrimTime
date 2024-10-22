5afdaf7651deb780ed695e93f03d3eaef932e55d
Deba43 <debadatta016@gmail.com>
2024-10-22 23:11:33 +0530

    rescheduleAppointment

--- src/main/java/com/debadatta/TrimTime/service/AppointmentsService.java
+++ src/main/java/com/debadatta/TrimTime/service/AppointmentsService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.debadatta.TrimTime.model.Appointments;
+import com.debadatta.TrimTime.model.Barbers;
 import com.debadatta.TrimTime.repo.AppointmentsRepo;
+import com.debadatta.TrimTime.repo.BarbersRepo;
 
 import lombok.AllArgsConstructor;
 
@@ -16,13 +18,38 @@ public class AppointmentsService {
 
     @Autowired
     private AppointmentsRepo appointmentsRepo;
+    @Autowired
+    private BarbersRepo barbersRepo;
 
-     public List<Appointments> getAllAppointments() {
+    public List<Appointments> getAllAppointments() {
         return appointmentsRepo.fetchAllAppointments();
     }
 
     public List<Appointments> getAppointmentsForBarber(String barber_id) {
         return appointmentsRepo.fetchAppointmentsForBarber(barber_id);
     }
-    
+
+    public boolean rescheduleAppointment(String appointment_id, Appointments updatedAppointments) {
+
+        Appointments existingAppointment = appointmentsRepo.getAppointmentId(appointment_id);
+        if (existingAppointment == null)
+            return false;
+
+        Barbers barber = barbersRepo.getBarbersById(updatedAppointments.getBarber_id());
+
+        if (barber != null && barber.isAvailable(updatedAppointments.getAppointment_date(),
+                updatedAppointments.getAppointment_time())) {
+
+            existingAppointment.setAppointment_date(updatedAppointments.getAppointment_date());
+            existingAppointment.setAppointment_time(updatedAppointments.getAppointment_time());
+            existingAppointment.setAppointment_status("Rescheduled");
+
+            appointmentsRepo.save(existingAppointment);
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
