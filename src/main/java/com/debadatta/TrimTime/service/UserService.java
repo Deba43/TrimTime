a14e82d1ccafe520361b1dbc4c771e8d87e15980
Deba43 <debadatta016@gmail.com>
2025-01-17 16:55:33 +0530

    code

--- src/main/java/com/debadatta/TrimTime/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.debadatta.TrimTime.service;
-
-import com.debadatta.TrimTime.dto.User;
-import com.debadatta.TrimTime.dto.UserRegistrationRequest;
-
-public interface UserService {
-    User registerUser(UserRegistrationRequest userRegistrationRequest);
-    User loginUser(String mobile_number, String otp);
-}
